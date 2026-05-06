d3d82a2b6e1a8c7b5d7cb19b73c2cb5713c984cb
mohammedirfanmf <mohammedirfan1948@gmail.com>
2026-05-06 14:23:44 +0530

    Update app.java

--- app.java
+++ app.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, SonarQube!");
+        String s = null;
+        System.out.println(s.length()); // NullPointerException
     }
 }
